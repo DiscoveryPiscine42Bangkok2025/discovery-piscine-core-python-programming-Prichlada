f43311c48dd0d7ab5ac34abaaec08a663b1e43a8
Prichlada <prichlada.r@gmail.com>
2026-04-28 17:28:12 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Prichlada"
+last_name = "Rachatrinn"
+print(first_name , last_name)
---
+++ cell01/ex01/name2.py
@@ -0,0 +1,4 @@
+first_name = "Prichlada"
+last_name = "Rachatrinn"
+whole_name = first_name + " " + last_name
+print(whole_name)
